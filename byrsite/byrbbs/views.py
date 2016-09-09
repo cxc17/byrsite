88cce736a6bd829d9bf07fdb9a922f7175046cfb
taveen_windows <caixiaochuan@sina.cn>
2016-09-09 17:28:58 +0800

    taveen 9/9

--- byrsite/byrbbs/views.py
+++ byrsite/byrbbs/views.py
@@ -3,12 +3,13 @@
 from django.shortcuts import render
 import time
 import urllib2
+import json
 
 from models import *
 
 
 def index(request):
-    if "key" in request.GET:
+    if "key" in request.GET and request.GET.get("key") != "":
         return search(request)
 
     return render(request, 'byrbbs/index.html')
@@ -58,9 +59,22 @@ def user(request):
     uid = request.GET.get("uid")
 
     url = "https://bbs.byr.cn/user/query/%s.json" % uid
-    r = urllib2.Request(url, headers={'cookie': 'nforum[UTMPUSERID]=oneseven; nforum[UTMPKEY]=94340691; nforum[UTMPNUM]=3185; nforum[PASSWORD]=psGXiAucyq0F3aa9j0ds1w%3D%3D; Hm_lvt_38b0e830a659ea9a05888b924f641842=1473085151; Hm_lpvt_38b0e830a659ea9a05888b924f641842=1473335368; nforum[XWJOKE]=hoho',
+    req = urllib2.Request(url, headers={'cookie': 'nforum[UTMPUSERID]=oneseven; nforum[PASSWORD]=psGXiAucyq0F3aa9j0ds1w%3D%3D;',
                                       'x-requested-with': 'XMLHttpRequest'})
-    a = urllib2.urlopen(r).read().decode("gbk")
-    print a
+    info = urllib2.urlopen(req).read()
+    try:
+        user_info = info.decode("gbk")
+    except:
+        user_info = info
 
-    return render(request, 'byrbbs/user_info.html')
+    try:
+        user_info = json.loads(user_info)
+        if user_info["gender"] == u'm':
+            user_info["gender"] = u'男生'
+        elif user_info["gender"] == u'n':
+            user_info["gender"] = u'女生'
+        user_info["last_login_time"] = time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(float(user_info["last_login_time"])))
+        user_info["status"] = user_info["status"].split(u"，")[0]
+    except:
+        return render(request, 'byrbbs/userInfo.html')
+    return render(request, 'byrbbs/userInfo.html', {'user_info': user_info})
